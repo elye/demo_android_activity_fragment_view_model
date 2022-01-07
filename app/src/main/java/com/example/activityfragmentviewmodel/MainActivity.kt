161660448f3fe68997d091a1a4710d8329c58558
elye <elye.project@gmail.com>
2022-01-07 14:07:40 +1100

    Add improve version of fragment and activity creation

--- app/src/main/java/com/example/activityfragmentviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/activityfragmentviewmodel/MainActivity.kt
@@ -2,16 +2,19 @@ package com.example.activityfragmentviewmodel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.add
 import com.example.activityfragmentviewmodel.ui.main.MainFragment
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity(R.layout.main_activity) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<MainFragment>(R.id.container)
+            }
         }
     }
 }
--- app/src/main/java/com/example/activityfragmentviewmodel/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/activityfragmentviewmodel/ui/main/MainFragment.kt
@@ -1,25 +1,9 @@
 package com.example.activityfragmentviewmodel.ui.main
 
-import androidx.lifecycle.ViewModelProvider
-import android.os.Bundle
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import com.example.activityfragmentviewmodel.R
 
 class MainFragment : Fragment(R.layout.main_fragment) {
-
-    companion object {
-        fun newInstance() = MainFragment()
-    }
-
-    private lateinit var viewModel: MainViewModel
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
+    private val viewModel by viewModels<MainViewModel>()
 }
