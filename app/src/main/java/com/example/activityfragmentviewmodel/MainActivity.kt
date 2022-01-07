b8d8035d419ce9bc3a9bb0933c23d60be9b1e05d
elye <elye.project@gmail.com>
2022-01-07 12:17:59 +1100

    Simplify creating layout

--- app/src/main/java/com/example/activityfragmentviewmodel/MainActivity.kt
+++ app/src/main/java/com/example/activityfragmentviewmodel/MainActivity.kt
@@ -4,11 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.activityfragmentviewmodel.ui.main.MainFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.main_activity) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.container, MainFragment.newInstance())
--- app/src/main/java/com/example/activityfragmentviewmodel/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/activityfragmentviewmodel/ui/main/MainFragment.kt
@@ -8,20 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.activityfragmentviewmodel.R
 
-class MainFragment : Fragment() {
+class MainFragment : Fragment(R.layout.main_fragment) {
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
     private lateinit var viewModel: MainViewModel
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
-    }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
